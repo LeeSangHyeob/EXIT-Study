54f61a58bcf281395635aca7bcfbe5559b748741
LeeSangHyeob <zerosmoon@naver.com>
2021-03-31 19:15:45 +0900

    Update [1672]maximumWealth.java

--- lsh/algorithm/leetcode/[1672]maximumWealth.java
+++ lsh/algorithm/leetcode/[1672]maximumWealth.java
@@ -1,17 +1,19 @@
 https://leetcode.com/problems/richest-customer-wealth/
 
 class Solution {
-    public int maximumWealth(int[][] accounts) {
-        int max = 0;
-		for(int i = 0; i < accounts.length; i++) {
+    public static int maximumWealth(int[][] accounts) {
+		
+		int max = 0;
+		//for(int i = 0; i < accounts.length; i++) {
+		for(int[] i: accounts) {
 			int sum = 0;
-			for(int j : accounts[i]) {
+			for(int j : i) {
 				sum += j;
 			}
 			if(max < sum) {
 				max = sum;
 			}
-		}
+		}		
 		return max;
-    }
+	}
 }
