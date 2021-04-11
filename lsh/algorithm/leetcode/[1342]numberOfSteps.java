cbbedaf36b2b427783ea6d0a23497e93717a6176
LeeSangHyeob <zerosmoon@naver.com>
2021-04-11 17:36:53 +0900

    Create [1342]numberOfSteps.java

---
+++ lsh/algorithm/leetcode/[1342]numberOfSteps.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int numberOfSteps(int num) {
+		int cnt = 0;
+		while(num > 0) {
+			if(num % 2 == 1) {
+				num -= 1;
+			} else {
+				num /= 2;
+			}
+			cnt++;
+		}
+		
+		return cnt;
+    }
+}
