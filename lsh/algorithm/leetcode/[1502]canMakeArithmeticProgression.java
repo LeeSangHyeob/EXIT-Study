1de37d36bdbe8ada2f0f28f9d33d700dd9efb613
LeeSangHyeob <zerosmoon@naver.com>
2021-04-18 17:30:30 +0900

    Update [1502]canMakeArithmeticProgression.java

--- lsh/algorithm/leetcode/[1502]canMakeArithmeticProgression.java
+++ lsh/algorithm/leetcode/[1502]canMakeArithmeticProgression.java
@@ -1,1 +1,14 @@
+https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/submissions/
 
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+		
+		int tmp = arr[1] - arr[0];
+		for(int i = 1; i < arr.length-1; i++) {
+			if(tmp != arr[i+1]-arr[i])
+				return false;
+		}
+		return true;
+    }
+}
