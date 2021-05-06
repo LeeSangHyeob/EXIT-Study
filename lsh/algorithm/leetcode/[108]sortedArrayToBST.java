d7488868cba550061fe42033a992801363159c08
LeeSangHyeob <zerosmoon@naver.com>
2021-05-06 12:18:02 +0900

    Create [108]sortedArrayToBST.java

---
+++ lsh/algorithm/leetcode/[108]sortedArrayToBST.java
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree
+
+public class sortedArrayToBST {
+
+	public static void main(String[] args) {
+		int[] nums = {-10,-3,0,5,9};
+		System.out.println(sortedArrayToBST(nums));
+	}
+	
+	public static TreeNode sortedArrayToBST(int[] nums) {
+		int size = nums.length;
+		return BinarySearchTree(nums, 0, size-1);
+	}
+	
+	public static TreeNode BinarySearchTree(int[] nums, int first, int last) {
+		if(first > last) return null;
+		int idx = first + (last - first) / 2;
+		TreeNode ans = new TreeNode(nums[idx]);
+		ans.left = BinarySearchTree(nums, first, idx-1);
+		ans.right = BinarySearchTree(nums, idx+1, last);
+		
+		return ans;
+	}
+
+}
