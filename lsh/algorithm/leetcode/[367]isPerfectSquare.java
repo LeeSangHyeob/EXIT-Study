c05a311654950f015ef28b39eac29ce586f91e47
LeeSangHyeob <zerosmoon@naver.com>
2021-08-19 09:46:24 +0900

    Update [367]isPerfectSquare.java

--- lsh/algorithm/leetcode/[367]isPerfectSquare.java
+++ lsh/algorithm/leetcode/[367]isPerfectSquare.java
@@ -2,6 +2,19 @@ https://leetcode.com/problems/valid-perfect-square/description/
 
 class Solution {
     public boolean isPerfectSquare(int num) {
-        return 0 == (double)Math.sqrt(num) % 1;
+        //return 0 == (double)Math.sqrt(num) % 1;
+        
+        long low = 1;
+        long high = num;
+        
+        while ( low <= high ){
+            long mid = (low + high) / 2;
+            
+            if(mid * mid == num) return true;
+            else if(mid * mid < num) low = mid + 1;
+            else if(mid * mid > num) high = mid - 1;
+        }
+        
+        return false;
     }
 }
