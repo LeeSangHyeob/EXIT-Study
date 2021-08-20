010edc3393849f0a0adc10c30144ed2d0cc10649
LeeSangHyeob <zerosmoon@naver.com>
2021-08-20 18:05:19 +0900

    Create [509]fib.java

---
+++ lsh/algorithm/leetcode/[509]fib.java
@@ -0,0 +1,8 @@
+https://leetcode.com/problems/fibonacci-number/description/
+
+class Solution {
+    public int fib(int n) {
+        if(n < 2) return n;
+        return fib(n-1) + fib(n-2);
+    }
+}
