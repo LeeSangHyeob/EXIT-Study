38ce3b7c5c15d75cf2bdd440f95c507e66ed0609
LeeSangHyeob <zerosmoon@naver.com>
2021-08-21 16:58:59 +0900

    Create [1929]getConcatenation.java

---
+++ lsh/algorithm/leetcode/[1929]getConcatenation.java
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+        
+        return ans;
+    }
+}
