9b2bb55b45f09363d8089ba5e90b9c3f012351b8
LeeSangHyeob <zerosmoon@naver.com>
2021-08-24 15:05:22 +0900

    Create [704]search.java

---
+++ lsh/algorithm/leetcode/[704]search.java
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/binary-search/description/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        
+        
+        while (low < high){
+            int mid = low + (high - low + 1) / 2;
+            if(nums[mid] > target) high = mid - 1;
+            else low = mid;
+        }
+        
+        return nums[low] == target? low : -1;
+    }
+}
