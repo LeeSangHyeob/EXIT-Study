986ff76208d9ca85dc213e20ce6dd23dae174b15
LeeSangHyeob <zerosmoon@naver.com>
2021-08-24 19:18:37 +0900

    Update [977]sortedSquares.java

--- lsh/algorithm/leetcode/[977]sortedSquares.java
+++ lsh/algorithm/leetcode/[977]sortedSquares.java
@@ -2,6 +2,7 @@ https://leetcode.com/problems/squares-of-a-sorted-array/description/
 
 class Solution {
     public int[] sortedSquares(int[] nums) {
+        /*
         for(int i = 0; i < nums.length; i++){
             nums[i] = nums[i] * nums[i];
         }
@@ -9,5 +10,22 @@ class Solution {
         Arrays.sort(nums);
         
         return nums;
+        */
+        
+        int[] ans = new int[nums.length];
+        int low = 0;
+        int high = nums.length - 1;
+        
+        for(int i = high; i >= 0; i--){
+            if(Math.abs(nums[low]) > Math.abs(nums[high])){
+                ans[i] = nums[low] * nums[low];
+                low++;
+            } else {
+                ans[i] = nums[high] * nums[high];
+                high--;
+            }
+        }
+        
+        return ans;
     }
 }
