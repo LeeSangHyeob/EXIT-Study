8aec5e70a0face9fc46814108501f2a7bdd3b67d
LeeSangHyeob <zerosmoon@naver.com>
2021-08-26 13:46:16 +0900

    Update BinaryGap.java

--- lsh/algorithm/codility/BinaryGap.java
+++ lsh/algorithm/codility/BinaryGap.java
@@ -2,33 +2,27 @@ https://app.codility.com/c/run/training7BHK34-5YP/
 
 public static int solution(int N) {
 
-		int max = 0;
-		int cnt = 0;
-		boolean chk = false;
-		
-		while (true) {
-			System.out.println(N + "/" + cnt + "/" + max);
-			switch (N % 2) {
-			case 0:
-				if(chk == true) {
-					cnt++;
-				}
-				break;
-			case 1:
-				chk = true;
-				if(cnt != 0) {
-					max = max < cnt ? cnt : max;
-					cnt = 0;
-				}
-				break;
-			}
+	int max = 0;
+	int cnt = 0;
+	boolean chk = false;
 
-			if (N == 1) {
-				break;
-			} else {
-				N /= 2;
+	while (N >= 1) {
+		System.out.println(N + "/" + cnt + "/" + max);
+		switch (N % 2) {
+		case 0:
+			if(chk == true) {
+				cnt++;
 			}
-
+			break;
+		case 1:
+			chk = true;
+			if(cnt != 0) {
+				max = max < cnt ? cnt : max;
+				cnt = 0;
+			}
+			break;
 		}
-		return max;
+		N /= 2;
 	}
+	return max;
+}
