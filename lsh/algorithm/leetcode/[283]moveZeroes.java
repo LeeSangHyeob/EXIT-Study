2370f9f56e069cb9a315a3f591d10cd8fab7654d
LeeSangHyeob <zerosmoon@naver.com>
2021-08-26 18:12:49 +0900

    Update [283]moveZeroes.java

--- lsh/algorithm/leetcode/[283]moveZeroes.java
+++ lsh/algorithm/leetcode/[283]moveZeroes.java
@@ -3,14 +3,22 @@ https://leetcode.com/problems/move-zeroes/description/
 class Solution {
     public void moveZeroes(int[] nums) {
         int idx = 0;
-        for(int i : nums){
-            if(i != 0){
-                nums[idx++] = i;
-            }
-        }
+//         for(int i : nums){
+//             if(i != 0){
+//                 nums[idx++] = i;
+//             }
+//         }
         
-        for(int i = idx; i < nums.length; i++){
-            nums[i] = 0;
+//         for(int i = idx; i < nums.length; i++){
+//             nums[i] = 0;
+//         }
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int tmp = nums[idx];
+                nums[idx++] = nums[i];
+                nums[i] = tmp;
+            }
         }
     }
 }
