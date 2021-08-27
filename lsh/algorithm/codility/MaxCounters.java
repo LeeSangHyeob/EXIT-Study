ceed34231947eff010485e23af24f22b0f0e2948
LeeSangHyeob <zerosmoon@naver.com>
2021-08-27 17:29:59 +0900

    Update MaxCounters.java

--- lsh/algorithm/codility/MaxCounters.java
+++ lsh/algorithm/codility/MaxCounters.java
@@ -1,26 +1,22 @@
 https://app.codility.com/programmers/lessons/4-counting_elements/max_counters/start/
 
-import java.util.*;
-
 class Solution {
     public int[] solution(int N, int[] A) {
+        // write your code in Java SE 8
         int[] ans = new int[N];
-        Map<Integer, Integer> map = new HashMap<>();
         int max = 0;
         int currMax = 0;
         for(int i : A){
             if(i > N){
                 currMax = max;
-                ans = new int[N];
             } else {
-                ans[i-1]++;
-                map.put(i, map.getOrDefault(i, 0) + 1);
-                max = max > map.get(i)? max : map.get(i);
+                ans[i-1] = ans[i-1] > currMax? ans[i-1] + 1 : currMax + 1;
+                max = max > ans[i-1]? max : ans[i-1];
             }
         }
-
+        
         for(int i = 0; i < N; i++){
-            ans[i] += currMax;
+            ans[i] = currMax > ans[i]? currMax : ans[i];
         }
 
         return ans;
