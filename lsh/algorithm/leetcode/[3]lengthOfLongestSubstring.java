3ffa9569cfac226be092acba57151c56d0af3e30
LeeSangHyeob <zerosmoon@naver.com>
2021-08-31 19:35:58 +0900

    Update [3]lengthOfLongestSubstring.java

--- lsh/algorithm/leetcode/[3]lengthOfLongestSubstring.java
+++ lsh/algorithm/leetcode/[3]lengthOfLongestSubstring.java
@@ -34,5 +34,19 @@ class Solution {
         }
         return max > map.size()? max : map.size();
         
+        
+//         int i = 0, j = 0, max = 0;
+//         Set<Character> set = new HashSet<>();
+
+//         while (j < s.length()) {
+//             if (!set.contains(s.charAt(j))) {
+//                 set.add(s.charAt(j++));
+//                 max = Math.max(max, set.size());
+//             } else {
+//                 set.remove(s.charAt(i++));
+//             }
+//         }
+
+//         return max;
     }
 }
