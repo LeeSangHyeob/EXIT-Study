b1975f2e7a63315825426373a6f3bf313f6d848a
LeeSangHyeob <zerosmoon@naver.com>
2021-08-31 19:33:30 +0900

    Update [3]lengthOfLongestSubstring.java

--- lsh/algorithm/leetcode/[3]lengthOfLongestSubstring.java
+++ lsh/algorithm/leetcode/[3]lengthOfLongestSubstring.java
@@ -2,23 +2,37 @@ https://leetcode.com/problems/longest-substring-without-repeating-characters/des
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length() < 2) return s.length();
+//         if(s.length() < 2) return s.length();
         
+//         int max = 0;
+//         int val = 0;
+//         Map<Character, Integer> map = new HashMap<>();
+        
+//         for(int i = 0 ; i < s.length(); i++){
+//             if(map.containsKey(s.charAt(i))){
+//                 i = map.get(s.charAt(i));
+//                 max = max > val? max : val;
+//                 map.clear();
+//                 val = 0;
+//             } else {
+//                 map.put(s.charAt(i), i);
+//                 val++;
+//             }
+//         }
+//         return max > val? max : val;
         int max = 0;
-        int val = 0;
         Map<Character, Integer> map = new HashMap<>();
         
         for(int i = 0 ; i < s.length(); i++){
             if(map.containsKey(s.charAt(i))){
                 i = map.get(s.charAt(i));
-                max = max > val? max : val;
+                max = max > map.size()? max : map.size();
                 map.clear();
-                val = 0;
             } else {
                 map.put(s.charAt(i), i);
-                val++;
             }
         }
-        return max > val? max : val;
+        return max > map.size()? max : map.size();
+        
     }
 }
