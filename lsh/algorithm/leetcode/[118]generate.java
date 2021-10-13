7aaccc86b5e22d6d858cbba6c51b65c159e33c2c
LeeSangHyeob <zerosmoon@naver.com>
2021-10-13 17:34:34 +0900

    Create [118]generate.java

---
+++ lsh/algorithm/leetcode/[118]generate.java
@@ -0,0 +1,21 @@
+https://leetcode.com/problems/pascals-triangle/description/
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> tmp = new ArrayList<>();
+        
+        for(int i = 0; i < numRows; i++){
+            tmp.add(0, 1);
+            
+            for(int j = 1; j < tmp.size() - 1; j++){
+                tmp.set(j, tmp.get(j) + tmp.get(j+1));
+            }
+            
+            ans.add(new ArrayList<>(tmp));
+        }
+        
+        return ans;
+    }
+}
