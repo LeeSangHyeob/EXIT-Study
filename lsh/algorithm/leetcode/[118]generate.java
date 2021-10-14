3c342f1458d50b85192a4a784552e646ba58270e
LeeSangHyeob <zerosmoon@naver.com>
2021-10-14 11:28:37 +0900

    Update [118]generate.java

--- lsh/algorithm/leetcode/[118]generate.java
+++ lsh/algorithm/leetcode/[118]generate.java
@@ -7,12 +7,17 @@ class Solution {
         List<Integer> tmp = new ArrayList<>();
         
         for(int i = 0; i < numRows; i++){
-            tmp.add(0, 1);
+//             tmp.add(0, 1);
             
-            for(int j = 1; j < tmp.size() - 1; j++){
-                tmp.set(j, tmp.get(j) + tmp.get(j+1));
+//             for(int j = 1; j < tmp.size() - 1; j++){
+//                 tmp.set(j, tmp.get(j) + tmp.get(j+1));
+//             }
+            
+            for(int j = tmp.size() - 1; j >= 1; j--){
+                tmp.set(j, tmp.get(j) + tmp.get(j-1));
             }
             
+            tmp.add(1);
             ans.add(new ArrayList<>(tmp));
         }
         
