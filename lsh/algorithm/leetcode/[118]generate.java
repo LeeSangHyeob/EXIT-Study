48002ff0da998c7e8b2e6984b355a37d8d121e10
LeeSangHyeob <zerosmoon@naver.com>
2021-10-14 11:29:51 +0900

    Update [118]generate.java

--- lsh/algorithm/leetcode/[118]generate.java
+++ lsh/algorithm/leetcode/[118]generate.java
@@ -7,12 +7,15 @@ class Solution {
         List<Integer> tmp = new ArrayList<>();
         
         for(int i = 0; i < numRows; i++){
+//            Start -> End
 //             tmp.add(0, 1);
             
 //             for(int j = 1; j < tmp.size() - 1; j++){
 //                 tmp.set(j, tmp.get(j) + tmp.get(j+1));
 //             }
             
+//          End -> Start
+//          뒤집은 이유 : 정방향으로 add 시 List Index를 하나씩 미뤄야함
             for(int j = tmp.size() - 1; j >= 1; j--){
                 tmp.set(j, tmp.get(j) + tmp.get(j-1));
             }
