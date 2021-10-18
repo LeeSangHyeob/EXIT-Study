8f4616f6a8c6f76c895324cd86e13964891d5b07
LeeSangHyeob <zerosmoon@naver.com>
2021-10-18 11:30:17 +0900

    Update [62]uniquePaths.java

--- lsh/algorithm/leetcode/[62]uniquePaths.java
+++ lsh/algorithm/leetcode/[62]uniquePaths.java
@@ -12,7 +12,7 @@ class Solution {
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < m; j++) {
 				if(i == 0 || j == 0){
-                    arr[i][j] = 1;
+					arr[i][j] = 1;
 				} else {
 					arr[i][j] = arr[i-1][j] + arr[i][j-1];
 				}
