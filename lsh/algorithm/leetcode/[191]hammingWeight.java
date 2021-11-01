b24db059627ab80567c7fc63ad02cd952a79eecc
LeeSangHyeob <zerosmoon@naver.com>
2021-11-01 11:53:07 +0900

    Create [191]hammingWeight.java

---
+++ lsh/algorithm/leetcode/[191]hammingWeight.java
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/number-of-1-bits/description/
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int ans = 0;
+        
+        while(n != 0){
+            ans += n & 1;
+            n >>>= 1;
+        }
+        
+        return ans;
+    }
+}
