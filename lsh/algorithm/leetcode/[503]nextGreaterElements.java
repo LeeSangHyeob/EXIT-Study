e818f2b2d750f2b439b1cb6a84ca15a8dde38d80
LeeSangHyeob <zerosmoon@naver.com>
2021-11-09 11:31:09 +0900

    Create [503]nextGreaterElements.java

---
+++ lsh/algorithm/leetcode/[503]nextGreaterElements.java
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/next-greater-element-ii/description/
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n * 2; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]){
+                res[stack.pop()] = nums[i % n];
+            }
+            stack.push(i % n);
+        }
+        return res;
+    }
+}
