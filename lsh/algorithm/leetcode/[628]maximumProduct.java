f328bee81ed491d6b47c2ba49456da8b2a7c9a70
LeeSangHyeob <zerosmoon@naver.com>
2021-12-31 14:50:58 +0900

    Create [628]maximumProduct.java

---
+++ lsh/algorithm/leetcode/[628]maximumProduct.java
@@ -0,0 +1,12 @@
+https://leetcode.com/problems/maximum-product-of-three-numbers/description/
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        
+        Arrays.sort(nums);
+        int len = nums.length;
+        
+        return nums[0] * nums[1] * nums[len- 1] < nums[len - 1] * nums[len - 2] * nums[len - 3] ? nums[len - 1] * nums[len - 2] * nums[len - 3] : nums[0] * nums[1] * nums[len - 1];
+        
+    }
+}
