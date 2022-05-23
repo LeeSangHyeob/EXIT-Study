0697a66cb9dcee72a35d2217701c509855426f90
LeeSangHyeob <zerosmoon@naver.com>
2022-05-23 12:44:37 +0900

    Create [225]MyStack.java

---
+++ lsh/algorithm/leetcode/[225]MyStack.java
@@ -0,0 +1,38 @@
+https://leetcode.com/problems/implement-stack-using-queues/description/
+
+class MyStack {
+    
+    Queue<Integer> q = null;
+
+    public MyStack() {
+        q = new LinkedList<Integer>();
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        for(int i = 1; i < q.size(); i++){
+            q.add(q.remove());
+        }
+    }
+    
+    public int pop() {
+        return q.remove();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
