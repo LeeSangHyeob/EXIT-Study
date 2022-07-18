3356e88bd8b95c63847e026421da0d303393225a
LeeSangHyeob <zerosmoon@naver.com>
2022-07-18 18:09:44 +0900

    Update [1480]runningSum.java

--- lsh/algorithm/leetcode/[1480]runningSum.java
+++ lsh/algorithm/leetcode/[1480]runningSum.java
@@ -1,13 +1,25 @@
 https://leetcode.com/problems/running-sum-of-1d-array/
 
-public static int[] runningSum(int[] nums) {
-		int[] ans = new int[nums.length];
-		int size = nums.length;
-		ans[0] = nums[0];
-		
-		for(int i = 1; i < size; i++) {
-			ans[i] = ans[i-1] + nums[i];
-		}
-		
-		return ans;
+// 2022.03
+// public static int[] runningSum(int[] nums) {
+// 	int[] ans = new int[nums.length];
+// 	int size = nums.length;
+// 	ans[0] = nums[0];
+
+// 	for(int i = 1; i < size; i++) {
+// 		ans[i] = ans[i-1] + nums[i];
+// 	}
+
+// 	return ans;
+// }
+
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i-1];
+        }
+        
+        return nums;
+    }
 }
