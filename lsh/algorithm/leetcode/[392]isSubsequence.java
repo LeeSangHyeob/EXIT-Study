75ae31d06df8e981fc09934f11f6b6917c255ce2
LeeSangHyeob <zerosmoon@naver.com>
2022-07-26 12:39:10 +0900

    Update [392]isSubsequence.java

--- lsh/algorithm/leetcode/[392]isSubsequence.java
+++ lsh/algorithm/leetcode/[392]isSubsequence.java
@@ -12,3 +12,15 @@ class Solution {
     return false;
     }  
 }
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() == 0) return true;
+        int idx = 0;
+        for(char c : t.toCharArray()) {
+            if(c == s.charAt(idx)) idx++;
+            if(idx == s.length()) return true;
+        }
+        return false;
+    }
+}
