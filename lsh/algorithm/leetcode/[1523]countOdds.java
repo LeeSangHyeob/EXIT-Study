59c006e35219c8fa8820e7c66fda96244da0a39e
LeeSangHyeob <zerosmoon@naver.com>
2022-09-18 12:27:25 +0900

    Create [1523]countOdds.java

---
+++ lsh/algorithm/leetcode/[1523]countOdds.java
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+
+class Solution {
+    public int countOdds(int low, int high) {
+        if(low % 2 == 0 && high % 2 == 0) {
+            return (high-low) / 2;
+        }
+        
+        return (high-low) / 2 + 1;
+    }
+}
