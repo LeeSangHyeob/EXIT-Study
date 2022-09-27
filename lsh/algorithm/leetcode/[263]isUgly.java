537a31a71f41d340ff4e3acdd39cbd33aae4e790
LeeSangHyeob <zerosmoon@naver.com>
2022-09-27 20:51:06 +0900

    Create [263]isUgly.java

---
+++ lsh/algorithm/leetcode/[263]isUgly.java
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/ugly-number/description/
+
+class Solution {
+    public boolean isUgly(int n) {
+        
+        if(n < 1) return false;
+        while (n >= 2) {
+            if(n % 2 == 0) n /= 2;
+            else if(n % 3 == 0) n /= 3;
+            else if(n % 5 == 0) n /= 5;
+            else return false;
+        }
+        
+        return true;
+    }
+}
