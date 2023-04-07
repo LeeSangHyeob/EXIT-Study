e7c4076d9e76fc13608014b92c74a9076bf1ccab
LeeSangHyeob <zerosmoon@naver.com>
2023-04-07 12:35:48 +0900

    Create [1325]removeLeafNodes.java

---
+++ lsh/algorithm/leetcode/[1325]removeLeafNodes.java
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/delete-leaves-with-a-given-value/description/
+
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root.left != null) root.left = removeLeafNodes(root.left, target);
+        if(root.right != null) root.right = removeLeafNodes(root.right, target);
+
+        return root.left == root.right && root.val == target? null : root;
+    }
+}
