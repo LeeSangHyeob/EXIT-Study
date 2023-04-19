0d05904246aee2758ec8a2dfd3b66d42c7f60234
LeeSangHyeob <zerosmoon@naver.com>
2023-04-19 12:22:23 +0900

    Create [2154]findFinalValue.java

---
+++ lsh/algorithm/leetcode/[2154]findFinalValue.java
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/keep-multiplying-found-values-by-two/description/
+
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i : nums) {
+            set.add(i);
+        }
+
+        while (set.contains(original)) {
+            original *= 2;
+        }
+
+        return original;
+    }
+}
