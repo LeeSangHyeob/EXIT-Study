6073721d48733634818184f7b69b5980fc9b6a78
LeeSangHyeob <zerosmoon@naver.com>
2023-07-10 11:58:25 +0900

    Create [867]transpose.java

---
+++ lsh/algorithm/leetcode/[867]transpose.java
@@ -0,0 +1,19 @@
+https://leetcode.com/problems/transpose-matrix/
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] res = new int[n][m];
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++){
+                res[j][i] = matrix[i][j];
+            }
+        }
+
+        return res;
+    }
+}
